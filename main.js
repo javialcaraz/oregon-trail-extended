844e08f354f8012ee430652c86ecb9609bebe48b
Javier Alcaraz <javi.alcaraz14@gmail.com>
2020-07-10 20:45:31 -0400

    first

---
+++ main.js
@@ -0,0 +1,37 @@
+class Wagon {
+    constructor (capacity, passengers) {
+    this.capacity = capacity
+    this.passengers = passengers
+    }
+
+    getAvailableSeatCount () {
+
+    }
+    join () {
+
+    }
+    shouldQuarantine() {
+
+    }
+    totalFood() {
+
+    }
+}
+
+
+class Travelers {
+    constructor (name, food) {
+    this.name = name
+    this.food = food || 0
+    this.isHealthy = isHealthy || true
+    }
+
+    hunt (){
+        for (var i = 0; i < myVar.length; i += 2) {
+
+        }
+    }
+
+
+    eat() 
+}
