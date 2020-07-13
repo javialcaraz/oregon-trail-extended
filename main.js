e51101793f5f8c8cc41e2b2be0034aa7ad3c0a33
Javer Alcaraz <javi.alcaraz14@gmail.com>
2020-07-13 16:33:30 -0400

    final

--- main.js
+++ main.js
@@ -1,37 +1,54 @@
-class Wagon {
-    constructor (capacity, passengers) {
-    this.capacity = capacity
-    this.passengers = passengers
+class Traveler {
+    constructor(name,) {
+        this.name = name
+        this.food = 1
+        this.isHealthy = true
     }
 
-    getAvailableSeatCount () {
-
+    hunt() {
+        this.food += 2
     }
-    join () {
 
-    }
-    shouldQuarantine() {
+    eat() {
 
-    }
-    totalFood() {
+        if (this.food > 0) {
+            this.food--
+        } else {
+            this.isHealthy = false
+        }
 
     }
 }
 
 
-class Travelers {
-    constructor (name, food) {
-    this.name = name
-    this.food = food || 0
-    this.isHealthy = isHealthy || true
+class Wagon {
+    constructor(capacity,) {
+        this.capacity = capacity
+        this.passengers = []
     }
 
-    hunt (){
-        for (var i = 0; i < myVar.length; i += 2) {
+    getAvailableSeatCount() {
+        return this.capacity - this.passengers.length
+    }
 
+    join(traveler) {
+        if (this.getAvailableSeatCount() > 1) {
+            this.passengers.push(traveler)
         }
     }
+    shouldQuarantine() {
+        for (i = 0; i < this.passengers.length; i++) {
+            var passenger = this.passengers[i]
+            if (passenger.isHealthy === false) {
+                return true
+            }
+        } return false
+    }
 
-
-    eat() 
+    totalFood() {
+        var total = 0
+        for (i = 0; i < this.passengers.length; i++) {
+            return total++
+        }
+    }
 }
